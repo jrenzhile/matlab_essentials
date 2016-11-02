aa3cfb295e18d4b341125ca3a62124924c70f197
ren <jrenzhile@gmail.com>
2016-11-02 10:29:21 -0400

    display matrix

--- display_matrix.m
+++ display_matrix.m
@@ -1,10 +1,14 @@
-function display_matrix(mat)
+function display_matrix(mat, spec)
+
+if ~exist('spec', 'var')
+    spec = '%.2f';
+end
 
 [H, W] = size(mat);
 
 for i = 1:H
     for j = 1:W
-        fprintf('%.2f ', mat(i, j));
+        fprintf(spec, mat(i, j));
     end
     fprintf('\n');
 end
