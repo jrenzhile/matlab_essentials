ea0bb23e29c95c6c1b27afff9c46b9922fcdc3bf
ren <jrenzhile@gmail.com>
2017-02-23 11:02:23 -0500

    minor change

--- editGridScript.m
+++ editGridScript.m
@@ -1,9 +1,12 @@
-function editGridScript(scriptName)
+function editGridScript(loaddir, scriptName)
 
-if ~exist(sprintf('%s_submit.sh', scriptName), 'file')
+if ~exist(sprintf('%s/%s_submit.sh', loaddir, scriptName), 'file')
     error('File Does not Exist\n');
 end
 
+cur_dir = pwd;
+cd(loaddir);
 eval(sprintf('edit %s_submit.sh', scriptName));
 eval(sprintf('edit %s_grid_driver.sh', scriptName));
 eval(sprintf('edit %s_grid.m', scriptName));
+cd(cur_dir);
