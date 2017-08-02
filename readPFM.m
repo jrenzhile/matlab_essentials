60c6979d82884dbcefaecc672587bf943cb0ede5
Zhile Ren <jrenzhile@gmail.com>
2017-08-02 11:41:06 -0700

    sync

--- readPFM.m
+++ readPFM.m
@@ -4,15 +4,24 @@ function im = readPFM(infile)
 
 fid = fopen(infile, 'r');
 str = fgetl(fid);
+if sum(findstr(str, 'PF'))>0
+    c = 3;
+else
+    c = 2;
+end
 dims = fgetl(fid);
 scale = fgetl(fid);
 dims = sscanf(dims, '%d %d');
-scale = sscanf(scale, '%f');
-w = dims(1); h = dims(2);
-im = reshape(fread(fid,prod(dims),'float=>double',0,'n'),[w,h])';
-im = im(end:-1:1,:);
+w = dims(1); h = dims(2); 
+if c==3
+    
+    im = reshape(fread(fid,prod(dims*3),'float=>double',0,'n'),[w,h,3])';
+    im = im(end:-1:1,:, :);
+else
+    im = reshape(fread(fid,prod(dims),'float=>double',0,'n'),[w,h])';
+    im = im(end:-1:1,:);
+end
 fclose(fid);
-
 end
 
 
